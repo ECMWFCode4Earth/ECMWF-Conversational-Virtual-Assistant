e522c2b5dbaf2426ede6b4dc01eaed806faccd62
Frank L <liefra@gmail.com>
2020-06-06 10:09:02 +0700

    Revert property to constructor

--- cva-retrieval-pipeline/src/main/kotlin/com/_2horizon/cva/retrieval/confluence/ConfluenceSpacesRetriever.kt
+++ cva-retrieval-pipeline/src/main/kotlin/com/_2horizon/cva/retrieval/confluence/ConfluenceSpacesRetriever.kt
@@ -1,7 +1,7 @@
 package com._2horizon.cva.retrieval.confluence
 
 import com._2horizon.cva.retrieval.event.ConfluenceSpacesEvent
-import io.micronaut.context.annotation.Requires
+import io.micronaut.context.annotation.Value
 import io.micronaut.context.event.ApplicationEventPublisher
 import io.micronaut.context.event.StartupEvent
 import io.micronaut.runtime.event.annotation.EventListener
@@ -12,16 +12,18 @@ import javax.inject.Singleton
  * Created by Frank Lieber (liefra) on 2020-05-09.
  */
 @Singleton
-@Requires(property = "app.feature.retrieval-pipeline.confluence.spaces.enabled", value = "true")
 class ConfluenceSpacesRetriever(
     private val confluenceOperations: ConfluenceOperations,
-    private val applicationEventPublisher: ApplicationEventPublisher
+    private val applicationEventPublisher: ApplicationEventPublisher,
+    @Value("\${app.feature.retrieval-pipeline.confluence.spaces.enabled:false}") private val retrievalPipelineSpacesEnabled: Boolean
 ) {
     private val log = LoggerFactory.getLogger(javaClass)
 
     @EventListener
     fun onStartup(startupEvent: StartupEvent) {
+        if (retrievalPipelineSpacesEnabled){
             retrieveSpaces()
+        }
     }
 
     internal fun retrieveSpaces(): ConfluenceSpacesEvent {
